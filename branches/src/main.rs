4cd2333ac6cafc22a9a7906e0a86abb91ef114d8
Harry D <harry@drummail.co.uk>
2024-02-08 22:40:49 +0000

    Conditionals

---
+++ branches/src/main.rs
@@ -0,0 +1,19 @@
+
+
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("Condition was true");
+    } else {
+        println!("Condition was false");
+    }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("Number is divisible by 3");
+    } else {
+        println!("Number is not divisible by 4 or 3");
+    }
+}
